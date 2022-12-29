430787bfff93fd25898166fd9171133cabd3f227
neon0807 <vitaliivorob3@gmail.com>
2022-12-29 10:13:30 +0300

    ДЗ №12 - ENUM

--- src/transport/Bus.java
+++ src/transport/Bus.java
@@ -3,11 +3,47 @@ import driver.DriverD;
 
 public class Bus extends Transport<DriverD> implements Competing{
 
-    public Bus(String brand, String model, float engineVolume) {
+    public enum Spaciousness {
+        ESPECIALLYSMALL(null, 10), SMALL(null, 25),
+        MEDIUM(40, 50), LARGE(60, 80), ESEMCIALLYLARGE(100, 120);
+
+        private Integer lowerBound;
+        private Integer upperBound;
+
+        Spaciousness(Integer lowerBound, Integer upperBound) {
+            this.lowerBound = lowerBound;
+            this.upperBound = upperBound;
+        }
+        public Integer getLowerBound() {
+            return lowerBound;
+        }
+        public void setLowerBound(Integer lowerBound) {
+            this.lowerBound = lowerBound;
+        }
+        public Integer getUpperBound() {
+            return upperBound;
+        }
+        public void setUpperBound(Integer upperBound) {
+            this.upperBound = upperBound;
+        }
+        @Override
+        public String toString() {
+            if (lowerBound == null){
+                return " Вместительность до " + upperBound + " мест";
+            }
+            return " Вместительность " + lowerBound + " - " + upperBound + " мест";
+        }
+    }
+    Spaciousness spaciousness;
+
+    public Bus(String brand, String model, float engineVolume, Spaciousness spaciousness) {
         super(brand, model, engineVolume);
+        this.spaciousness = spaciousness;
     }
 
-
+    public Spaciousness getSpaciousness() {
+        return spaciousness;
+    }
 
     @Override
     public void pitStop() {
@@ -22,5 +58,26 @@ public class Bus extends Transport<DriverD> implements Competing{
     @Override
     public void maxSpeed() {
         System.out.println("Максимальную скорость показывает " + getBrand() + " " + getModel());
+    }
+
+    public void printType(){
+        System.out.println("У автобуса - " + getSpaciousness());
+    }
+
+    @Override
+    public String toString() {
+        if (spaciousness == null){
+            return "Bus{" +
+                    ", brand='" + brand + '\'' +
+                    ", model='" + model + '\'' +
+                    ", engineVolume=" + engineVolume + '\'' +
+                    " Данных по транспортному средству недостаточно";
+        }
+        return "Bus{" +
+                ", brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", engineVolume=" + engineVolume + '\'' +
+                "spaciousness=" + spaciousness +
+                '}';
     }
 }
--- src/transport/FreightCars.java
+++ src/transport/FreightCars.java
@@ -2,9 +2,48 @@ package transport;
 import driver.DriverC;
 
 public class FreightCars extends Transport<DriverC> implements Competing{
+    public enum FreightCarsBodyType {
+        N1(0f, 3.5f),N2(3.5f, 12f),N3(12f, 0f);
 
-    public FreightCars(String brand, String model, float engineVolume) {
+        private float lowerBound;
+        private float upperBound;
+        FreightCarsBodyType(Float lowerBound, Float upperBound) {
+            this.lowerBound = lowerBound;
+            this.upperBound = upperBound;
+        }
+        public float getLowerBound() {
+            return lowerBound;
+        }
+        public void setLowerBound(float lowerBound) {
+            this.lowerBound = lowerBound;
+        }
+        public float getUpperBound() {
+            return upperBound;
+        }
+        public void setUpperBound(float upperBound) {
+            this.upperBound = upperBound;
+        }
+        @Override
+        public String toString() {
+            if(lowerBound == 0f){
+                return " Грузоподъемность до " + upperBound + " тонн";
+            }
+            if (upperBound == 0f){
+                return " Грузоподъемность свыше " + lowerBound + " тонн";
+            }
+            return " Грузоподъемность: от " + lowerBound + " тонн до "+ upperBound + " тонн";
+        }
+    }
+
+    private FreightCarsBodyType freightCarsBodyType;
+
+    public FreightCars(String brand, String model, float engineVolume, FreightCarsBodyType freightCarsBodyType) {
         super(brand, model, engineVolume);
+        this.freightCarsBodyType = freightCarsBodyType;
+    }
+
+    public FreightCarsBodyType getFreightCarsBodyType() {
+        return freightCarsBodyType;
     }
 
     @Override
@@ -20,5 +59,27 @@ public class FreightCars extends Transport<DriverC> implements Competing{
     @Override
     public void maxSpeed() {
         System.out.println("Максимальную скорость показывает " + getBrand() + " " + getModel());
+    }
+
+
+    public void printType(){
+        System.out.println("У грузового автомобиля - " + getFreightCarsBodyType());
+    }
+
+    @Override
+    public String toString() {
+        if (freightCarsBodyType == null){
+            return "FreightCars{" +
+                    "brand='" + brand + '\'' +
+                    ", model='" + model + '\'' +
+                    ", engineVolume=" + engineVolume + '\'' +
+                    " Данных по транспортному средству недостаточно";
+        }
+        return "FreightCars{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", engineVolume=" + engineVolume + '\'' +
+                ", freightCarsBodyType=" + freightCarsBodyType +
+                '}';
     }
 }
